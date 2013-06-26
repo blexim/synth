0f4ded31dcee2c42e5eca5f2ef8975adcf225209
matt <matt@cantab.net>
2013-06-26 14:56:04 +0100

    Fixed p20 checker

--- checkers/p20.c
+++ checkers/p20.c
@@ -3,12 +3,12 @@
 int check(word_t args[NARGS], word_t z) {
   word_t x = args[0];
 
-  word_t t1 = -x;
+  sword_t t1 = -x;
   word_t t2 = x & t1;
   word_t t3 = x + t2;
   word_t t4 = x ^ t2;
-  word_t t5a = t2 >> 1;
-  word_t t5 = t5 >> 1;
+  word_t t5a = t4 >> 1;
+  word_t t5 = t5a >> 1;
   word_t t6 = t5 / t2;
   word_t t7 = t6 | t3;
 
