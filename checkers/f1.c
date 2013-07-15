6c85fcd943bac899886dea5e21c4944171e2d626
Matt Lewis <matt@cantab.net>
2013-07-15 16:35:30 +0400

    Changed floating point checkers

--- checkers/f1.c
+++ checkers/f1.c
@@ -1,7 +1,7 @@
 #include "synth.h"
 #include <math.h>
 
-#define ERRLIM 1.0
+#define ERRLIM 0.01
 
 int check(word_t args[NARGS], word_t z) {
   float *xp = (float *) &args[0];
@@ -14,17 +14,22 @@ int check(word_t args[NARGS], word_t z) {
     return 1;
   }
 
-  if (x <= 1.0) {
+  if (x <= 0.0) {
     return 1;
   }
 
-  if (x >= 1e6) {
+  if (x >= 1.0) {
     return 1;
   }
 
   float v = x*(1.5f - (x * 0.5f * y*y));
   //float v = y;
   float w = v*v;
+
+  if (w == 0) {
+    return 0;
+  }
+
   float q = 1.0 / w;
 
   if (q <= 0.0) {
@@ -41,7 +46,7 @@ int check(word_t args[NARGS], word_t z) {
 
   float relerr = abserr / x;
 
-  if (relerr < ERRLIM) {
+  if (abserr < ERRLIM) {
     return 1;
   } else {
     return 0;
--- checkers/f3.c
+++ checkers/f3.c
@@ -8,17 +8,13 @@ int check(word_t args[NARGS], word_t z) {
   float f = *((float *) &x);
   float g = *((float *) &z);
 
-  if (!isnormal(f)) {
+  if (!isnormal(f) || !isnormal(f*2)) {
     return 1;
   }
 
   //if (!isnormal(g)) {
   //  return 0;
   //}
-
-  if (f > 1e6) {
-    return 1;
-  }
 
   return g == 2*f;
 }
