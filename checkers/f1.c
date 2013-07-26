37a2711e45a88841f8f7db35292deefe4f892dee
matt <matt@cantab.net>
2013-07-26 14:53:06 +0100

    Changed f1 checker

--- checkers/f1.c
+++ checkers/f1.c
@@ -3,12 +3,19 @@
 
 #define ERRLIM 0.01
 
-int check(word_t args[NARGS], word_t z) {
-  float *xp = (float *) &args[0];
-  float x = (float) args[0];
+union ia {
+  word_t i;
+  float f;
+};
 
-  float *yp = (float *) &z;
-  float y = (float) z;
+int check(word_t args[NARGS], word_t z) {
+  union ia ia;
+
+  ia.i = args[0];
+  float x = ia.f;
+
+  ia.i = z;
+  float y = ia.f;
 
   if (!isnormal(x)) {
     return 1;
@@ -28,29 +35,9 @@ int check(word_t args[NARGS], word_t z) {
 
   float v = x*(1.5f - (x * 0.5f * y*y));
   //float v = y;
-  float w = v*v;
+  float res = v*v*x;
 
-  if (w == 0) {
-    return 0;
-  }
-
-  float q = 1.0 / w;
-
-  if (q <= 0.0) {
-    return 0;
-  }
-
-  float abserr;
-
-  if (x > q) {
-    abserr = x - q;
-  } else {
-    abserr = q - x;
-  }
-
-  float relerr = abserr / x;
-
-  if (abserr < ERRLIM) {
+  if ((res >= 1.0 - ERRLIM) && (res <= 1.0 + ERRLIM)) {
     return 1;
   } else {
     return 0;
