be3b7b8f5544a407f00a8f575f7fdad8e838f60c
matt <matt@cantab.net>
2013-09-29 17:31:56 +0100

    Fix bug in spec p22

--- specs/p22.c
+++ specs/p22.c
@@ -2,15 +2,15 @@
 
 int check(word_t args[NARGS], word_t res[NRES]) {
   word_t x = args[0];
-  word_t res = 0;
-  word_t bit;
   word_t z = res[0];
+  word_t expected = 0;
+  word_t bit;
 
   for (int i = 0; i < WIDTH; i++) {
     bit = x & 1;
-    res ^= bit;
+    expected ^= bit;
     x >>= 1;
   }
 
-  return res == z;
+  return expected == z;
 }
