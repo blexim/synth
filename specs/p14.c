f077409b70ad542f9e759a3d98785bb94c29efa0
matt <matt@cantab.net>
2013-09-29 17:32:08 +0100

    Changes spec p14 to be more declarative

--- specs/p14.c
+++ specs/p14.c
@@ -4,15 +4,15 @@ int check(word_t args[NARGS], word_t res[NRES]) {
   word_t x = args[0];
   word_t y = args[1];
   word_t z = res[0];
+  word_t z2 = 2 * z;
+  word_t sum = x + y;
+  word_t sum_minus_1 = sum - 1;
 
-  word_t t1 = x & y;
-  word_t t2 = x ^ y;
-  word_t t3 = t2 >> 1;
-  word_t t4 = t1 + t3;
+  if ((z <= x && z >= y) || (z <= y && z >= x)) {
+    if ((z2 == sum) || (z2 == sum_minus_1)) {
+      return 1;
+    }
+  }
 
-#ifdef SEARCH
-  t4 &= ((1 << WIDTH) - 1);
-#endif
-
-  return z == t4;
+  return 0;
 }
