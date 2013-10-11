e3026ca3964fbe7f4f44e0bfb9105ed6e20f3d05
Matt Lewis <matt@cantab.net>
2013-10-11 03:34:54 +0100

    Added ICSE table

--- paper/figures/icse_table.py
+++ paper/figures/icse_table.py
@@ -7,30 +7,33 @@ benchmark_dir = "../../benchmarks"
 
 # each data point is (runtime (s), runtime semibiased standard, runtime semibiased ext., std = ext., #lines)
 brahma = [
-    (1.48, 0.8, 0.8, True, 2),
-    (7.35, 4.75, 4.75, True, 2),
-    (1.6, 0.65, 0.65, True, 2),
-    (1.65, 0.86, 0.86, True, 2),
-    (3.92, 2.28, 2.28, True, 2),
-    (6.22, 1.64, 1.64, True, 2),
-    (1.39, .5, .5, True, 3),
-    (2.2, 1.42, 1.42, True, 3),
-    (4.95, 3.85, 4.9, False, 3),
-    (13.99, 4.57, 3.25, False, 3),
-    (24.31, 2.86, 14.27, False, 3),
-    (279.49, 2.64, 45.52, False, 3),
-    (32.5, 3.02, 6.95, False, 4),
-    (14.32, 3, 3.66, False, 4),
-    (167.84, 4.5, 13.57, False, 4),
-    (66.93, 4.95, 18.97, False, 4),
-    (228.78, 7.98, 25.55, False, 4),
-    (163.82, 65.45, 65.45, True, 4),
-    (214.14, 19.3, 63.23, False, 6),
-    (1074.04, 13.28, 272.28, False, 7),
-    (3600, 187.17, 185.57, False, 12)
+    (1.48, 0.8, 0.8, True, 2),   #1
+    (7.35, 4.75, 4.75, True, 2), #2
+    (1.6, 0.65, 0.65, True, 2), #3
+    (1.65, 0.86, 0.86, True, 2), #4
+    (3.92, 2.28, 2.28, True, 2), #5
+    (6.22, 1.64, 1.64, True, 2), #6
+    (1.39, .5, .5, True, 3),     #7
+    (2.2, 1.42, 1.42, True, 3),  #8
+    (4.95, 3.85, 4.9, False, 3), #9
+    (13.99, 4.57, 3.25, False, 3), #10
+    (24.31, 2.86, 14.27, False, 3), #11
+    (279.49, 2.64, 45.52, False, 3), #12
+    (32.5, 3.02, 6.95, False, 4), #13
+    (14.32, 3, 3.66, False, 4),   #14
+    (167.84, 4.5, 13.57, False, 4), #15
+    (66.93, 4.95, 18.97, False, 4), #16
+    (217.34, 5.89, 20.62, False, 4), #17
+    (228.78, 7.98, 25.55, False, 4), #18
+    (163.82, 65.45, 65.45, True, 4), #19
+    (214.14, 19.3, 63.23, False, 6), #22
+    (1074.04, 13.28, 272.28, False, 7), #21
+    (3600, 187.17, 185.57, False, 12),  #22
+    (1.38, 12.12, 12.12, True, 3),   #23
+    (5.28, 2.96, 2.96, True, 4)  #24
 ]
 
-mappings = [
+mappings = dict([
     (1, 1),
     (2, 2),
     (3, 3),
@@ -55,8 +58,8 @@ mappings = [
     (22, 24),
     (23, 28),
     (24, 29),
-    (25, 30)
-]
+    #(25, 30)
+])
 
 def load_file(filename):
   counters = {}
@@ -117,28 +120,33 @@ def load_file(filename):
   return (counters, timers)
 
 def make_graph():
-  kalashnikov = [load_file("%s/%d.out" % (benchmark_dir, i))
+  kalashnikov = [load_file("%s/%d.out" % (benchmark_dir, mappings[i]))
       for i in xrange(1, len(brahma) + 1)]
 
-  print "\\begin{tabular}{l||rrr|rrr}"
-  print "Problem & \multicolumn{3}{c}{\\sc Brahma} & \multicolumn{3}{|c}{\\sc Kalashnikov} \\\\"
-  print "        & Iter. & Runtime & \#Lines & Iter. & Runtime & \#Lines \\\\"
+  print "\\begin{tabular}{l||rr|rr|rr}"
+  print "Problem & \multicolumn{2}{c}{Random {\\sc Brahma}} & \multicolumn{2}{|c}{Semibiased {\sc Brahma}} & \multicolumn{2}{|c}{\\sc Kalashnikov} \\\\"
+  print "        & Runtime & \#Lines & Runtime & \#Lines & Runtime & \#Lines \\\\"
 
   print "\\hline"
   print "\\hline"
 
   for i in xrange(len(brahma)):
-    (brit, brt, brinsts) = brahma[i]
+    (brt, brstt, brext, breq, brl) = brahma[i]
     (kalcnt, kaltimes) = kalashnikov[i]
 
-    mintime = min(brt, kaltimes['_'])
-    maxinsts = max(brinsts, kalcnt['insts'])
+    if breq:
+      semitime = brstt
+    else:
+      semitime = brstt + brext
+
+    mintime = min(brt, kaltimes['_'], semitime)
+    maxinsts = max(brl, kalcnt['insts'])
 
     line = "p%d & " % (i+1)
 
 
-    def f(t, its, insts):
-      ret = '%d & ' % its
+    def f(t, insts):
+      ret = ''
 
       if t == mintime:
         ret += "{\\bf %.02fs} &" % t
@@ -156,8 +164,9 @@ def make_graph():
 
       return ret
 
-    line += f(brt, brit, brinsts)
-    line += f(kaltimes['_'], kalcnt['iterations'], kalcnt['insts'])
+    line += f(brt, brl)
+    line += f(semitime, brl)
+    line += f(kaltimes['_'], kalcnt['insts'])
 
     line = line[:-1]
 
