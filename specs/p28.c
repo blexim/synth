67823504867e2f68760cff36cd39e31c3ff15075
Matt Lewis <matt@cantab.net>
2013-10-11 02:15:23 +0100

    Added spec for ICSE #23

---
+++ specs/p28.c
@@ -0,0 +1,39 @@
+#include "synth.h"
+
+
+void interchangeObs(word_t* src, word_t* dest)
+{
+  *src = *src ^ *dest;
+
+  if (*src == *src ^ *dest) {
+    *src = *src ^ *dest;
+
+    if (*src == *src ^ *dest) {
+      *dest = *src ^ *dest;
+      
+      if (*dest == *src ^ *dest) {
+        *src = *dest ^ *src;
+        return;
+      } else {
+        *src = *src ^ *dest;
+        *dest = *src ^ *dest;
+        return;
+      } 
+    } else
+      *src = *src ^ *dest;
+  }
+  
+  *dest = *src ^ *dest;
+  *src = *src ^ *dest;
+
+  return;
+}
+
+int check(word_t args[2], word_t res[2]) {
+  word_t src = args[0];
+  word_t dest = args[1];
+
+  interchangeObs(&src, &dest);
+
+  return (src == res[0] && dest == res[1]);
+}
