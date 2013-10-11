077b8fffae0c647e367bfd14c8b4d537f52e0a33
Matt Lewis <blexim@gmail.com>
2013-10-11 02:16:46 +0100

    Fix some specs

--- specs/p19.c
+++ specs/p19.c
@@ -6,6 +6,18 @@ int check(word_t args[NARGS], word_t res[NRES]) {
   word_t k = args[2];
   word_t z = res[0];
 
+  if (k >= WIDTH) {
+    return 1;
+  }
+
+  if (m >= (1 << (WIDTH - k - 1))) {
+    return 1;
+  }
+
+  if (m == 0) {
+    return 1;
+  }
+
   word_t t1 = x >> k;
   word_t t2 = x ^ t1;
   word_t t3 = t2 & m;
@@ -13,5 +25,5 @@ int check(word_t args[NARGS], word_t res[NRES]) {
   word_t t5 = t4 ^ t3;
   word_t t6 = t5 ^ x;
 
-  return t6 == z;
+  return z == t6;
 }
--- specs/p20.c
+++ specs/p20.c
@@ -6,11 +6,25 @@ int check(word_t args[NARGS], word_t res[NRES]) {
 
   sword_t t1 = -x;
   word_t t2 = x & t1;
+
+  if (t2 == 0) {
+    return 1;
+  }
+
   word_t t3 = x + t2;
   word_t t4 = x ^ t2;
   word_t t5 = t4 >> 2;
+
+#ifdef SEARCH
+  t5 &= ((1 << WIDTH) - 1);
+#endif
+
   word_t t6 = t5 / t2;
   word_t t7 = t6 | t3;
+
+#ifdef SEARCH
+  t7 &= ((1 << WIDTH) -1);
+#endif
 
   return z == t7;
 }
