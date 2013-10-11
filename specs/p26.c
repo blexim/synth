02780d78efa71d0fbad8e415b8cc51d590da20df
Matt Lewis <matt@cantab.net>
2013-10-11 01:56:07 +0100

    Add spec for ICSE 14

---
+++ specs/p26.c
@@ -0,0 +1,18 @@
+#include "synth.h"
+
+int check(word_t args[2], word_t res[1]) {
+  word_t x = args[0];
+  word_t k = args[1];
+  word_t z = res[0];
+
+  word_t t1 = (-1 >> k);
+  word_t t2 = t1 + 1;
+  word_t t3 = x - t2;
+  word_t t4 = t3 & t1;
+
+#ifdef SEARCH
+  t4 &= ((1 << WIDTH) - 1);
+#endif
+
+  return z == t4;
+}
