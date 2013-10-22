a05182cc63c5326a5b03e208f53d5a3e597a310f
Matt Lewis <matt@cantab.net>
2013-10-22 17:09:20 +0100

    Rename to Kalashnikov

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -35,7 +35,7 @@ def runbenchmarks():
     else:
       word = 4
 
-    cmd = ("./cegar.py -a%d -r%d -w%d -v specs/p%d.c > benchmarks/%d.out" %
+    cmd = ("./kalashnikov.py -a%d -r%d -w%d -v specs/p%d.c > benchmarks/%d.out" %
         (args, res, word, i, i))
 
     print cmd
