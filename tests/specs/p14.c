67e93ebaf540c38ec07beb96b5f3e45c02e21991
matt <matt@cantab.net>
2014-05-11 12:44:52 +0100

    Rephrase p14

--- tests/specs/p14.c
+++ tests/specs/p14.c
@@ -7,15 +7,11 @@ int check(prog_t *prog, word_t args[2]) {
   word_t x = args[0];
   word_t y = args[1];
   word_t z = res[0];
-  word_t z2 = 2 * z;
-  word_t sum = x + y;
-  word_t sum_minus_1 = sum - 1;
 
-  if ((z <= x && z >= y) || (z <= y && z >= x)) {
-    if ((z2 == sum) || (z2 == sum_minus_1)) {
-      return 1;
-    }
-  }
+  word_t t1 = x & y;
+  word_t t2 = x ^ y;
+  word_t t3 = t2 >> 1;
+  word_t t4 = t1 + t3;
 
-  return 0;
+  return t4 == z;
 }
