0026c244f4930f250fad0dd79dc1c29b43b5c552
matt <matt@cantab.net>
2014-05-24 16:04:08 +0100

    Added reachability skeleton

---
+++ tests/loops/reachability.c
@@ -0,0 +1,55 @@
+#include "synth.h"
+
+extern void prefix(word_t args[NARGS]);
+extern int guard(word_t args[NARGS]);
+extern void body(word_t args[NARGS]);
+extern int assertion(word_t args[NARGS]);
+
+int inv(prog_t *prog, word_t args[NARGS]) {
+  word_t res;
+
+  exec(prog, args, &res);
+
+  return res;
+}
+
+int check(solution_t *solution, word_t args[NARGS]) {
+  word_t vars[NARGS];
+  prog_t *prog = &solution->prog;
+  int i;
+
+  for (i = 0; i < NARGS; i++) {
+    vars[i] = solution->evars[i];
+  }
+
+  prefix(vars);
+
+  if (!inv(prog, vars)) {
+    return 0;
+  }
+
+  for (i = 0; i < NARGS; i++) {
+    vars[i] = args[i];
+  }
+
+  if (guard(vars) && inv(prog, vars)) {
+    body(vars);
+
+    if (!inv(prog, vars)) {
+      return 0;
+    }
+  }
+
+  for (i = 0; i < NARGS; i++) {
+    vars[i] = args[i];
+  }
+
+  if (!guard(vars) && inv(prog, vars)) {
+    if (assertion(vars)) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
