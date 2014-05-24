f2c92a4fe9dc3ec7f4fe23a4dd51f642ea191b22
matt <matt@cantab.net>
2014-05-24 16:04:18 +0100

    Added count-up-down specs from sv-comp14

---
+++ tests/loops/count_up_down_false-components.c
@@ -0,0 +1,19 @@
+#include "synth.h"
+
+void prefix(word_t vars[3]) {
+  vars[0] = vars[2];
+  vars[1] = 0;
+}
+
+int guard(word_t vars[3]) {
+  return vars[0] > 0;
+}
+
+void body(word_t vars[3]) {
+  vars[0]--;
+  vars[1]++;
+}
+
+int assertion(word_t vars[3]) {
+  return vars[1] != vars[2];
+}
