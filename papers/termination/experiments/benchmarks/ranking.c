afccbaf17c44e9e003b4e93385156da58c281b24
Matt <matt@cantab.net>
2014-06-13 14:24:32 +0100

    Have ranking skeleton use lexicographic ranking functions

--- papers/termination/experiments/benchmarks/ranking.c
+++ papers/termination/experiments/benchmarks/ranking.c
@@ -5,12 +5,22 @@ extern int guard(word_t in_vars[NARGS]);
 extern void body(word_t in_vars[NARGS], word_t out_vars[NARGS]);
 extern int assertion(word_t args[NARGS]);
 
-word_t rank(prog_t *prog, word_t args[NARGS]) {
-  word_t res;
+void rank(prog_t *prog, word_t args[NARGS], word_t res[NRES]) {
+  exec(prog, args, res);
+}
 
-  exec(prog, args, &res);
+int cmp(word_t rank1[NRES], word_t rank2[NRES]) {
+  int i;
 
-  return res;
+  for (i = 0; i < NRES; i++) {
+    if (rank1[i] < rank2[i]) {
+      return -1;
+    } else if (rank1[i] > rank2[i]) {
+      return 1;
+    }
+  }
+
+  return 0;
 }
 
 int check(solution_t *solution, word_t args[NARGS]) {
@@ -23,7 +33,10 @@ int check(solution_t *solution, word_t args[NARGS]) {
   }
 
   if (guard(pre_vars)) {
-    word_t r1 = rank(prog, pre_vars);
+    word_t r1[NRES];
+    word_t r2[NRES];
+
+    rank(prog, pre_vars, r1);
 
     if (r1 <= 0) {
       return 0;
@@ -31,9 +44,9 @@ int check(solution_t *solution, word_t args[NARGS]) {
 
     body(pre_vars, post_vars);
 
-    word_t r2 = rank(prog, post_vars);
+    rank(prog, post_vars, r2);
 
-    if (r1 <= r2) {
+    if (cmp(r1, r2) <= 0) {
       return 0;
     }
   }
