85fb26972190308f2dc0f48a5fc47f0bc04d66e2
matt <matt@cantab.net>
2014-06-27 16:42:44 +0100

    Solution loader

--- src/kalashnikov/lib/io.c
+++ src/kalashnikov/lib/io.c
@@ -6,9 +6,21 @@
 #include "synth.h"
 
 #define TESTFILE "/tmp/testvectors"
+#define SOLFILE "/tmp/solution"
 
 unsigned int **test_vectors;
 int numtests;
+
+#define getline(f) do { \
+  fgets(buf, sizeof(buf), f); \
+  p = buf; \
+} while (0)
+
+#define getint(x) do { \
+  (x) = strtol(p, &q, 0); \
+  assert(q > p); \
+} while(0)
+
 
 void load_tests() {
   FILE *testfile = fopen(TESTFILE, "r");
@@ -22,26 +34,20 @@ void load_tests() {
   // <test 1>
   // <test 2>
   // ...
-  fgets(buf, sizeof(buf) - 1, testfile);
-  numtests = strtol(buf, &p, 0);
-  assert(p > buf);
+  getline(testfile);
+  getint(numtests);
 
   test_vectors = malloc(numtests * sizeof(int *));
 
   for (int i = 0; i < numtests; i++) {
     test_vectors[i] = malloc(NARGS * sizeof(int));
 
-    fgets(buf, sizeof(buf) - 1, testfile);
-
-    p = buf;
+    getline(testfile);
 
     printf("Test vector %d: ", i);
 
     for (int j = 0; j < NARGS; j++) {
-      test_vectors[i][j] = strtol(p, &q, 0);
-      assert(q > p);
-      p = q;
-
+      getint(test_vectors[i][j]);
       printf("%d ", test_vectors[i][j]);
     }
 
@@ -59,4 +65,55 @@ void free_tests() {
   }
 
   free(test_vectors);
+}
+
+void load_solution(solution_t *solution) {
+  FILE *solfile = fopen(SOLFILE, "r");
+  assert(solfile != NULL);
+
+  memset(solution, 0, sizeof(solution_t));
+
+  char buf[1024];
+  char *p, *q;
+
+  // Format of the file is:
+  //
+  // <evar1> <evar2> ...
+  // <n1-instructions>
+  // <opcode1> <param11> <param12> <param13>
+  // ...
+  // <const1>
+  // <const2>
+  // ...
+  // <n2-instructions><n2-consts>
+  // ...
+
+  getline(solfile);
+
+  for (int i = 0; i < NEVARS; i++) {
+    getint(solution->evars[i]);
+  }
+
+  for (int i = 0; i < NPROGS; i++) {
+    prog_t *prog = &solution->progs[i];
+
+    getline(solfile);
+    getint(prog->len);
+
+    for (int j = 0; j < prog->len; j++) {
+      getline(solfile);
+
+      getint(prog->ops[j]);
+      getint(prog->params[3*j]);
+      getint(prog->params[3*j + 1]);
+      getint(prog->params[3*j + 2]);
+    }
+
+    for (int j = 0; j < CONSTS; j++) {
+      getline(solfile);
+      getint(prog->consts[j]);
+    }
+  }
+
+  fclose(solfile);
 }
--- src/kalashnikov/lib/io.h
+++ src/kalashnikov/lib/io.h
@@ -1,10 +1,14 @@
 #ifndef IO_H
 #define IO_H
 
+#include "synth.h"
+
 extern unsigned int **test_vectors;
 extern int numtests;
 
 void load_tests();
 void free_tests();
+
+void load_solution(solution_t *solution);
 
 #endif
