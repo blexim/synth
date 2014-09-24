97d9a067a85e9d66f55cc956596bd58706d26d36
matt <matt@cantab.net>
2014-09-24 12:36:30 +0100

    Heap verification harness

---
+++ tests/heap/heapverif.c
@@ -0,0 +1,25 @@
+#include "heaptheory.h"
+#include "synth.h"
+
+#define x 0
+#define y 1
+#define z 2
+
+int main(void) {
+  word_t pre[NARGS];
+  word_t post[NARGS];
+
+  if (well_formed(pre)) {
+    update(pre, post, x, y);
+    assert(well_formed(post));
+
+    assign(pre, post, x, y);
+    assert(well_formed(post));
+
+    lookup(pre, post, x, y);
+    assert(well_formed(post));
+
+    alloc(pre, post, x);
+    assert(well_formed(post));
+  }
+}
