1894fb840b8c48c75d1669e3ee88e0b32e3f64d5
Matt <matt@cantab.net>
2014-09-28 15:46:00 +0100

    Heap abstraction function & verifier

---
+++ tests/heap/heapabstract.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+#ifndef NNODES
+ #define NNODES 5
+#endif
+
+#define NMATRIX NNODES*NNODES
+#define ABSSIZE NMATRIX
+
+#ifndef NPROG
+ #define NPROG 2
+#endif
+
+#define INF 0xffffffff
+
+#define idx(x, y) (x*NNODES + y)
+
+#define min(x, y) (x < y ? x : y)
+
+void abstract(unsigned int graph[NMATRIX],
+              unsigned int abstraction[ABSSIZE]) {
+  int i, x;
+
+  for (i = 0; i < NMATRIX; i++) {
+    abstraction[i] = INF;
+  }
+
+  for (x = 0; x < NNODES; x++) {
+    abstraction[idx(x, x)] = 0;
+  }
+
+  for (i = 0; i < NNODES; i++) {
+    for (x = 0; x < NMATRIX; x++) {
+      if (graph[x]) {
+        abstraction[x] = min(i, abstraction[x]);
+      }
+    }
+  }
+}
+
+int is_valid_heap(unsigned int graph[NMATRIX]) {
+  int x, y, cnt;
+
+  for (x = 0; x < NNODES; x++) {
+    cnt = 0;
+
+    for (y = 0; y < NNODES; y++) {
+      if (graph[idx(x, y)] != 0 &&
+          graph[idx(x, y)] != 1) {
+        return 0;
+      }
+
+      if (graph[idx(x, y)]) {
+        cnt++;
+      }
+    }
+
+    if (cnt > 1) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+void reachable(unsigned int graph[NMATRIX],
+               unsigned int out[NNODES]) {
+  int i, x, y;
+
+  for (i = 0; i < NPROG; i++) {
+    out[i] = 1;
+  }
+
+  for (i = NPROG; i < NNODES; i++) {
+    out[i] = 0;
+  }
+
+  for (i = 0; i < NNODES; i++) {
+    for (x = 0; x < NNODES; x++) {
+      for (y = 0; y < NNODES; y++) {
+        if (out[x] && graph[idx(x, y)]) {
+          out[y] = 1;
+        }
+      }
+    }
+  }
+}
+
+int heaps_equal(unsigned int graph1[NMATRIX],
+                unsigned int graph2[NMATRIX]) {
+  unsigned int reaches1[NNODES], reaches2[NNODES];
+
+  reachable(graph1, reaches1);
+  reachable(graph2, reaches2);
+
+  int x, y;
+
+  for (x = 0; x < NNODES; x++) {
+    if (reaches1[x] != reaches2[x]) {
+      return 0;
+    }
+
+    for (y = 0; y < NNODES; y++) {
+      if (reaches1[x] && reaches1[y]) {
+        if (graph1[idx(x, y)] != graph2[idx(x, y)]) {
+          return 0;
+        }
+      }
+    }
+  }
+
+  return 1;
+}
+
+int abstractions_equal(unsigned int abs1[ABSSIZE],
+                       unsigned int abs2[ABSSIZE]) {
+  int i;
+
+  for (i = 0; i < ABSSIZE; i++) {
+    if (abs1[i] != abs2[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int main(void) {
+  unsigned int heap1[NMATRIX], heap2[NMATRIX];
+  unsigned int abs1[ABSSIZE], abs2[ABSSIZE];
+
+  abstract(heap1, abs1);
+  abstract(heap2, abs2);
+
+  if (abstractions_equal(abs1, abs2) &&
+      is_valid_heap(heap1) &&
+      is_valid_heap(heap2)) {
+    assert(heaps_equal(heap1, heap2));
+  }
+}
