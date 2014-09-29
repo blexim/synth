6459f55d94dcaba52e466f68a74be19a208bd8ea
matt <matt@cantab.net>
2014-09-29 19:47:04 +0100

    Improve abstract lookup

--- tests/heap/abstract_transformers.c
+++ tests/heap/abstract_transformers.c
@@ -1,5 +1,32 @@
 #include "heapabstract.h"
 
+/*
+ * Saturating addition.
+ */
+unsigned int s_add(unsigned int x, unsigned int y) {
+  unsigned int ret = (x > INF - y) ? INF : x + y;
+
+  __CPROVER_assume(ret != INF || x == INF || y == INF);
+
+  return ret;
+}
+
+/*
+ * Saturating subtraction.
+ */
+unsigned int s_sub(unsigned int x, unsigned int y) {
+  if (x == INF) {
+    return INF;
+  } else if (y > x) {
+    return 0;
+  } else {
+    return y - x;
+  }
+}
+
+/*
+ * Copy an abstract heap.
+ */
 void copy_abstract(abstract_heapt *pre,
                    abstract_heapt *post) {
   word_t x, y;
@@ -16,12 +43,15 @@ void copy_abstract(abstract_heapt *pre,
   }
 }
 
+/*
+ * The abstract transformer for assignment:
+ *
+ * x = y;
+ */
 void abstract_assign(word_t x,
                      word_t y,
                      abstract_heapt *pre,
                      abstract_heapt *post) {
-  int i;
-
   copy_abstract(pre, post);
 
   unsigned int z;
@@ -43,4 +73,54 @@ void abstract_assign(word_t x,
   post->dist[x][x] = 0;
   post->cut[x][x] = 0;
   post->cut_cut[x][x] = 0;
+}
+
+void abstract_lookup(word_t x,
+                     word_t y,
+                     abstract_heapt *pre,
+                     abstract_heapt *post) {
+  copy_abstract(pre, post);
+
+  word_t a;
+  word_t len;
+
+  for (a = 0; a < NPROG; a++) {
+    // First we work out the distance x -> a
+    if (a == x) {
+      // a == x, and x always aliases itself.
+      post->dist[x][a] = 0;
+    } else if (pre->dist[a][y] == 0) {
+      // a is an alias of y...
+      if (pre->stem[y] == 0) {
+        // y is part of a cycle of length k, so x -> a == x -> y == k-1
+        post->dist[x][a] = s_sub(pre->cycle[y], 1);
+      } else {
+        // y cannot reach y, and so x cannot.
+        post->dist[x][a] = INF;
+      }
+    } else {
+      // Otherwise, the distance y -> a is k, and so x -> a = k-1
+      post->dist[x][a] = s_sub(pre->dist[y][a], 1);
+    }
+
+    // Now work out the distance a -> x
+    if (a == x) {
+      post->dist[x][a] = 0;
+    } else if (pre->cut[y][a] == 1 && pre->dist[y][a] == INF) {
+      // y is one step away from a cutpoint with a.  There is a bit of trickery
+      // here -- we have to remember to account for the distance between the ay
+      // cutpoint and the ya cutpoint.
+      post->dist[a][x] = s_add(pre->cut[a][y], pre->cut_cut[a][y]);
+    } else if (pre->cut[y][a] == 1 && pre->cycle[a] != INF) {
+      // y is one step away from a cycle containing a.
+      len = s_sub(pre->cycle[a], pre->dist[y][a]);
+      len = s_add(len, 1);
+      post->dist[a][x] = len;
+    } else if (pre->dist[y][a] == 1) {
+      post->dist[a][x] = 0;
+    } else {
+      // Otherwise, the distance a -> y is k, so a -> x = k+1
+      post->dist[a][x] = s_add(pre->dist[a][y], 1);
+    }
+  }
 }
