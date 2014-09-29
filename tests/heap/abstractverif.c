461818967d0b9bb36238d3bc3010bcd6bbd07b9e
matt <matt@cantab.net>
2014-09-29 16:12:24 +0100

    Refactor heap abstraction verifier

--- tests/heap/abstractverif.c
+++ tests/heap/abstractverif.c
@@ -2,14 +2,28 @@
 
 int main(void) {
   concrete_heapt heap1, heap2;
+
+  assert(check(heap1, heap2));
+}
+
+int check(concrete_heapt heap1, concrete_heapt heap2) {
+  concrete_heapt heap1, heap2;
   abstract_heapt abs1, abs2;
+
+  if (!is_valid_heap(heap1) || !is_valid_heap(heap2)) {
+    return 1;
+  }
+
+  if (heaps_isomorphic(heap1, heap2)) {
+    return 1;
+  }
 
   abstract(heap1, abs1);
   abstract(heap2, abs2);
 
-  if (abstractions_equal(abs1, abs2) &&
-      is_valid_heap(heap1) &&
-      is_valid_heap(heap2)) {
-    assert(heaps_isomorphic(heap1, heap2));
+  if (!abstractions_equal(abs1, abs2)) {
+    return 1;
   }
+
+  return 0;
 }
