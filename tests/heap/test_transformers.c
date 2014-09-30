033b05db8f1006d3c30758a7cc56480f45b27d3b
matt <matt@cantab.net>
2014-09-30 02:31:22 +0100

    Heap udpate test harness

--- tests/heap/test_transformers.c
+++ tests/heap/test_transformers.c
@@ -144,9 +144,59 @@ void test_lookups() {
 #endif
 }
 
+void test_update(concrete_heapt *heap1) {
+  concrete_heapt heap2;
+  abstract_heapt abs1, abs2, abs3;
+
+  abstract(heap1, &abs1);
+  abstract_update(x, y, &abs1, &abs2);
+
+  concrete_update(x, y, heap1, &heap2);
+  abstract(&heap2, &abs3);
+
+  printf("Concrete heap1:\n");
+  print_concrete(heap1);
+
+  printf("\nAbstract1:\n");
+  print_abstract(&abs1);
+
+  printf("\nExecuting x->n = y:\n");
+
+  printf("\nAbstract result:\n");
+  print_abstract(&abs2);
+
+  printf("\nConcrete result:\n");
+  print_concrete(&heap2);
+
+  printf("\nAbstracted concrete:\n");
+  print_abstract(&abs3);
+
+  tests++;
+
+  if (is_valid_heap(heap1) &&
+      !abstractions_equal(&abs2, &abs3)) {
+    printf("TEST FAILED\n");
+  } else {
+    passed++;
+    printf("TEST SUCCEEDED\n");
+  }
+}
+
+void test_updates() {
+#if NNODES==3
+  concrete_heapt heap1 = {
+    .succ={ INF, 1, 1 }, .ptr={ 0, 2, 0 }
+  };
+  test_update(&heap1);
+#elif NNODES==4
+#elif NNODES==5
+#endif
+}
+
 void main() {
   //test_assigns();
-  test_lookups();
+  //test_lookups();
+  test_updates();
 
   printf("\n%d/%d tests passed\n", passed, tests);
 }
