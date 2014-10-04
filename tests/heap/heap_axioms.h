aeab8aed34a94cce068d130c91ca6c16dc47aa25
matt <matt@cantab.net>
2014-10-04 11:04:11 +0100

    Add all_axioms prototype

--- tests/heap/heap_axioms.h
+++ tests/heap/heap_axioms.h
@@ -6,6 +6,7 @@ int null_axioms(abstract_heapt *heap);
 int cycle_axioms(abstract_heapt *heap);
 int cut_axioms(abstract_heapt *heap);
 int cut_cut_axioms(abstract_heapt *heap);
+int all_axioms(abstract_heapt *heap);
 
 int acyclic(abstract_heapt *heap);
 int no_sharing(abstract_heapt *heap);
