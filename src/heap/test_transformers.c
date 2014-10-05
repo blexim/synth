997e8eee33b23a3bd922a26816464348521abab2
matt <matt@cantab.net>
2014-10-05 20:30:36 +0100

    Transformer tests

--- src/heap/test_transformers.c
+++ src/heap/test_transformers.c
@@ -2,11 +2,11 @@
 
 int main(void) {
   abstract_heapt heap1 = {
-    .succ={ 0, 1, 1, 2, 3 }, .dist={ 0, 1, 4, 1, 1 }, .ptr={ 0, 1 },
-          .nnodes=2
+    .succ={ 0, 4, 3, 5, 6, 3, 6 }, .dist={ 0, 9, 1, 9, 4, 9, 1 }, .ptr={ 0, 1, 4 },
+        .nnodes=0
   };
   ptr_t x = 1;
-  ptr_t y = 0;
+  ptr_t y = 2;
   abstract_heapt heap2;
 
 
@@ -15,10 +15,10 @@ int main(void) {
 
   //__CPROVER_assume(valid_abstract_heap(&heap1));
 
-  abstract_new(&heap1, &heap2, x);
+  abstract_lookup(&heap1, &heap2, x, y);
 
   print_abstract_heap(&heap1);
-  printf("\nx = new();\n");
+  printf("\nx = y->n;\n");
   print_abstract_heap(&heap2);
 
   printf("Heap 1 is valid: %d\n", valid_abstract_heap(&heap1));
--- src/heap/verify_transformers.c
+++ src/heap/verify_transformers.c
@@ -3,16 +3,16 @@
 int main(void) {
   abstract_heapt heap1, heap2;
 
-  ptr_t x;
-  ptr_t y;
+  ptr_t x = 1;
+  ptr_t y = 2;
 
   __CPROVER_assume(x < NPROG && x > 0);
   __CPROVER_assume(y < NPROG && x > 0);
 
   __CPROVER_assume(valid_abstract_heap(&heap1));
 
-  abstract_new(&heap1, &heap2, x);
-  assert(valid_abstract_heap(&heap2));
+  //abstract_new(&heap1, &heap2, x);
+  //assert(valid_abstract_heap(&heap2));
 
   //abstract_lookup(&heap1, &heap2, x, y);
   //assert(valid_abstract_heap(&heap2));
@@ -20,6 +20,6 @@ int main(void) {
   //abstract_assign(&heap1, &heap2, x, y);
   //assert(valid_abstract_heap(&heap2));
 
-  //abstract_update(&heap1, &heap2, x, y);
-  //assert(valid_abstract_heap(&heap2));
+  abstract_update(&heap1, &heap2, x, y);
+  assert(valid_abstract_heap(&heap2));
 }
