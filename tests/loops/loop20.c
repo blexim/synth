c21b298c8e74940b59b0815ec2c86017069ee422
Matt <matt@cantab.net>
2015-02-14 15:53:54 +0000

    Make loop20 bigger

--- tests/loops/loop20.c
+++ tests/loops/loop20.c
@@ -1,10 +1,17 @@
+/*
+ * Danger invariant:
+ *  x_0 = y_0 = nondet_0 = 0
+ *  D(nondet0, x, y) = x == y
+ *  R(nondet0, x, y) = x
+ *  nondet(x, y) = 1
+ */
 int main(void) {
   unsigned int x, y;
 
   x = 0;
   y = 0;
 
-  while (x < 10) {
+  while (x < 1000) {
     x++;
 
     if (nondet()) {
