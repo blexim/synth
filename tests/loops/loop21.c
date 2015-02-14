687c88c00f5c6d5456575cfba378ece867e8daa3
Matt <matt@cantab.net>
2015-02-14 16:11:13 +0000

    Loop21 is a harder version of loop20

---
+++ tests/loops/loop21.c
@@ -0,0 +1,24 @@
+/*
+ * Danger invariant:
+ *  x_0 = y_0 = nondet_0 = 0
+ *  D(nondet0, x, y) = y == (x ? x : 1)
+ *  R(nondet0, x, y) = x
+ *  nondet(x, y) = x
+ */
+int main(void) {
+  unsigned int x, y;
+
+  x = 0;
+  y = 1;
+
+  while (x < 1000) {
+    x++;
+
+    if (nondet()) {
+      y++;
+    }
+  }
+
+  assert(x != y);
+}
+
