f53bf762cd346ceec2dc73c2384bbe9da523bb33
Matt <matt@cantab.net>
2015-02-14 14:43:55 +0000

    Fix a bug in reachability template

--- tests/loops/reachability.c
+++ tests/loops/reachability.c
@@ -1,20 +1,20 @@
 #include "synth.h"
 
-extern void prefix(word_t args[NARGS]);
+extern void prefix(word_t args[NARGS], word_t out_args[NARGS]);
 extern int guard(word_t args[NARGS]);
-extern void body(word_t args[NARGS]);
+extern void body(word_t args[NARGS], word_t out_args[NARGS]);
 extern int assertion(word_t args[NARGS]);
 
 int inv(prog_t *prog, word_t args[NARGS]) {
-  word_t res;
+  word_t res[NRES];
 
-  exec(prog, args, &res);
+  exec(prog, args, res);
 
-  return res;
+  return res[0];
 }
 
 int check(solution_t *solution, word_t args[NARGS]) {
-  word_t vars[NARGS];
+  word_t vars[NARGS], post_vars[NARGS];
   prog_t *prog = &solution->progs[0];
   int i;
 
@@ -22,9 +22,9 @@ int check(solution_t *solution, word_t args[NARGS]) {
     vars[i] = solution->evars[i];
   }
 
-  prefix(vars);
+  prefix(vars, post_vars);
 
-  if (!inv(prog, vars)) {
+  if (!inv(prog, post_vars)) {
     return 0;
   }
 
@@ -33,9 +33,9 @@ int check(solution_t *solution, word_t args[NARGS]) {
   }
 
   if (guard(vars) && inv(prog, vars)) {
-    body(vars);
+    body(vars, post_vars);
 
-    if (!inv(prog, vars)) {
+    if (!inv(prog, post_vars)) {
       return 0;
     }
   }
