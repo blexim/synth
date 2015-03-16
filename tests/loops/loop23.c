4ab3a8832bbb122b0fc4fac85ea345c16f7179c4
matt <matt@cantab.net>
2015-03-16 21:00:24 +0000

    Unsafe loop and danger logs for 30 and 33

--- tests/loops/loop23.c
+++ tests/loops/loop23.c
@@ -1,3 +1,14 @@
+/*
+ * An unsafe loop, modelling a check for a buffer overflow.
+ *
+ * Danger invariant:
+ *
+ *  x_0 = 3221225472
+ *  i_0 = len_0 = 0
+ *  D(x, len, i) = i == 0 && x != 0 && len == 0
+ *  R(x, len, i) = true
+ */
+
 int main(void) {
   unsigned int x;
   unsigned int len;
